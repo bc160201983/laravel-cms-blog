4bb2f6c4fba9221fec5fbcf39b56d575c5eb4f11
Bilal Afzaal <bc160201983@vu.edu.pk>
2019-04-20 19:54:20 +0500

    home template strucher

---
+++ resources/views/layouts/homeapp.blade.php
@@ -0,0 +1,388 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<head>
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Favicon-->
+	<link rel="shortcut icon" href="img/fav.png">
+	<!-- Author Meta -->
+	<meta name="author" content="codepixer">
+	<!-- Meta Description -->
+	<meta name="description" content="">
+	<!-- Meta Keyword -->
+	<meta name="keywords" content="">
+	<!-- meta character set -->
+	<meta charset="UTF-8">
+	<!-- Site Title -->
+	<title>Ruft Blog</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,500|Rubik:500" rel="stylesheet">
+	<!--
+			CSS
+			============================================= -->
+			<link href="{{ asset('css/linearicons.css') }}" rel="stylesheet">
+			<link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
+			<link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+			<link href="{{ asset('css/magnific-popup.css') }}" rel="stylesheet">
+			<link href="{{ asset('css/nice-select.css') }}" rel="stylesheet">
+			<link href="{{ asset('css/animate.min.css') }}" rel="stylesheet">
+			<link href="{{ asset('css/owl.carousel.css') }}" rel="stylesheet">
+			<link href="{{ asset('css/main.css') }}" rel="stylesheet">
+</head>
+
+<body>
+	<!--================ Start header Top Area =================-->
+        @include('inc.homeheader')
+	<!-- End header Area -->
+
+	<!-- Top Stories Area -->
+	<section class="top-stories-area">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="owl-carousel active-stories-carousel">
+						<!-- single stories carousel -->
+						<div class="single-stories-carousel d-flex align-items-center">
+							<div class="stories-thumb">
+								<img class="img-fluid" src="img/posts/carousel/stories1.jpg" alt="">
+							</div>
+							<div class="stories-details">
+								<h6><a href="">2nd Gen Smoke Alarm <br>
+										get up from sleep</a></h6>
+								<p>September 14, 2018</p>
+							</div>
+						</div>
+						<!-- single stories carousel -->
+						<div class="single-stories-carousel d-flex align-items-center">
+							<div class="stories-thumb">
+								<img class="img-fluid" src="img/posts/carousel/stories2.jpg" alt="">
+							</div>
+							<div class="stories-details">
+								<h6><a href="">2nd Gen Smoke Alarm <br>
+										get up from sleep</a></h6>
+								<p>September 14, 2018</p>
+							</div>
+						</div>
+						<!-- single stories carousel -->
+						<div class="single-stories-carousel d-flex align-items-center">
+							<div class="stories-thumb">
+								<img class="img-fluid" src="img/posts/carousel/stories3.jpg" alt="">
+							</div>
+							<div class="stories-details">
+								<h6><a href="">2nd Gen Smoke Alarm <br>
+										get up from sleep</a></h6>
+								<p>September 14, 2018</p>
+							</div>
+						</div>
+						<!-- single stories carousel -->
+						<div class="single-stories-carousel d-flex align-items-center">
+							<div class="stories-thumb">
+								<img class="img-fluid" src="img/posts/carousel/stories4.jpg" alt="">
+							</div>
+							<div class="stories-details">
+								<h6><a href="">2nd Gen Smoke Alarm <br>
+										get up from sleep</a></h6>
+								<p>September 14, 2018</p>
+							</div>
+						</div>
+						<!-- single stories carousel -->
+						<div class="single-stories-carousel d-flex align-items-center">
+							<div class="stories-thumb">
+								<img class="img-fluid" src="img/posts/carousel/stories5.jpg" alt="">
+							</div>
+							<div class="stories-details">
+								<h6><a href="">2nd Gen Smoke Alarm <br>
+										get up from sleep</a></h6>
+								<p>September 14, 2018</p>
+							</div>
+						</div>
+						<!-- single stories carousel -->
+						<div class="single-stories-carousel d-flex align-items-center">
+							<div class="stories-thumb">
+								<img class="img-fluid" src="img/posts/carousel/stories6.jpg" alt="">
+							</div>
+							<div class="stories-details">
+								<h6><a href="">2nd Gen Smoke Alarm <br>
+										get up from sleep</a></h6>
+								<p>September 14, 2018</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End Stories Area -->
+
+	<!-- Start Post Silder Area -->
+        @include('inc.postslider')
+	<!-- Start Post Silder Area -->
+
+	<!-- Start main body Area -->
+	<div class="main-body section-gap">
+		<div class="container box_1170">
+			<div class="row">
+				<div class="col-lg-8 post-list">
+					<!-- Start Post Area -->
+					<section class="post-area">
+						<div class="row">
+							<div class="col-lg-6 col-md-6">
+								<div class="single-post-item">
+									<div class="post-thumb">
+										<img class="img-fluid" src="img/archive/c1.jpg" alt="">
+									</div>
+									<div class="post-details">
+										<h4><a href="#">An Stormy Evening</a></h4>
+										<p>There is a moment in the life of any aspiring astronomer that it is time to buy that first telescope. It’s
+											exciting to think about setting up your own viewing station.</p>
+										<div class="blog-meta">
+											<a href="#" class="m-gap"><span class="lnr lnr-calendar-full"></span>March 14, 2018</a>
+											<a href="#" class="m-gap"><span class="lnr lnr-bubble"></span>05</span></a>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-lg-6 col-md-6">
+								<div class="single-post-item">
+									<div class="post-thumb">
+										<img class="img-fluid" src="img/archive/c2.jpg" alt="">
+									</div>
+									<div class="post-details">
+										<h4><a href="#">An Stormy Evening</a></h4>
+										<p>There is a moment in the life of any aspiring astronomer that it is time to buy that first telescope. It’s
+											exciting to think about setting up your own viewing station.</p>
+										<div class="blog-meta">
+											<a href="#" class="m-gap"><span class="lnr lnr-calendar-full"></span>March 14, 2018</a>
+											<a href="#" class="m-gap"><span class="lnr lnr-bubble"></span>05</span></a>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-lg-6 col-md-6">
+								<div class="single-post-item">
+									<div class="post-thumb">
+										<img class="img-fluid" src="img/archive/c3.jpg" alt="">
+									</div>
+									<div class="post-details">
+										<h4><a href="#">An Stormy Evening</a></h4>
+										<p>There is a moment in the life of any aspiring astronomer that it is time to buy that first telescope. It’s
+											exciting to think about setting up your own viewing station.</p>
+										<div class="blog-meta">
+											<a href="#" class="m-gap"><span class="lnr lnr-calendar-full"></span>March 14, 2018</a>
+											<a href="#" class="m-gap"><span class="lnr lnr-bubble"></span>05</span></a>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-lg-6 col-md-6">
+								<div class="single-post-item">
+									<div class="post-thumb">
+										<img class="img-fluid" src="img/archive/c4.jpg" alt="">
+									</div>
+									<div class="post-details">
+										<h4><a href="#">An Stormy Evening</a></h4>
+										<p>There is a moment in the life of any aspiring astronomer that it is time to buy that first telescope. It’s
+											exciting to think about setting up your own viewing station.</p>
+										<div class="blog-meta">
+											<a href="#" class="m-gap"><span class="lnr lnr-calendar-full"></span>March 14, 2018</a>
+											<a href="#" class="m-gap"><span class="lnr lnr-bubble"></span>05</span></a>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-lg-6 col-md-6">
+								<div class="single-post-item">
+									<div class="post-thumb">
+										<img class="img-fluid" src="img/archive/c1.jpg" alt="">
+									</div>
+									<div class="post-details">
+										<h4><a href="#">An Stormy Evening</a></h4>
+										<p>There is a moment in the life of any aspiring astronomer that it is time to buy that first telescope. It’s
+											exciting to think about setting up your own viewing station.</p>
+										<div class="blog-meta">
+											<a href="#" class="m-gap"><span class="lnr lnr-calendar-full"></span>March 14, 2018</a>
+											<a href="#" class="m-gap"><span class="lnr lnr-bubble"></span>05</span></a>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-lg-6 col-md-6">
+								<div class="single-post-item">
+									<div class="post-thumb">
+										<img class="img-fluid" src="img/archive/c2.jpg" alt="">
+									</div>
+									<div class="post-details">
+										<h4><a href="#">An Stormy Evening</a></h4>
+										<p>There is a moment in the life of any aspiring astronomer that it is time to buy that first telescope. It’s
+											exciting to think about setting up your own viewing station.</p>
+										<div class="blog-meta">
+											<a href="#" class="m-gap"><span class="lnr lnr-calendar-full"></span>March 14, 2018</a>
+											<a href="#" class="m-gap"><span class="lnr lnr-bubble"></span>05</span></a>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-lg-6 col-md-6">
+								<div class="single-post-item">
+									<div class="post-thumb">
+										<img class="img-fluid" src="img/archive/c3.jpg" alt="">
+									</div>
+									<div class="post-details">
+										<h4><a href="#">An Stormy Evening</a></h4>
+										<p>There is a moment in the life of any aspiring astronomer that it is time to buy that first telescope. It’s
+											exciting to think about setting up your own viewing station.</p>
+										<div class="blog-meta">
+											<a href="#" class="m-gap"><span class="lnr lnr-calendar-full"></span>March 14, 2018</a>
+											<a href="#" class="m-gap"><span class="lnr lnr-bubble"></span>05</span></a>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-lg-6 col-md-6">
+								<div class="single-post-item">
+									<div class="post-thumb">
+										<img class="img-fluid" src="img/archive/c4.jpg" alt="">
+									</div>
+									<div class="post-details">
+										<h4><a href="#">An Stormy Evening</a></h4>
+										<p>There is a moment in the life of any aspiring astronomer that it is time to buy that first telescope. It’s
+											exciting to think about setting up your own viewing station.</p>
+										<div class="blog-meta">
+											<a href="#" class="m-gap"><span class="lnr lnr-calendar-full"></span>March 14, 2018</a>
+											<a href="#" class="m-gap"><span class="lnr lnr-bubble"></span>05</span></a>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-lg-12">
+								<nav class="blog-pagination justify-content-center d-flex">
+									<ul class="pagination">
+										<li class="page-item">
+											<a href="#" class="page-link" aria-label="Previous">
+												<span aria-hidden="true">
+													<span class="lnr lnr-arrow-left"></span>
+												</span>
+											</a>
+										</li>
+										<li class="page-item"><a href="#" class="page-link">01</a></li>
+										<li class="page-item active"><a href="#" class="page-link">02</a></li>
+										<li class="page-item"><a href="#" class="page-link">03</a></li>
+										<li class="page-item"><a href="#" class="page-link">04</a></li>
+										<li class="page-item"><a href="#" class="page-link">09</a></li>
+										<li class="page-item">
+											<a href="#" class="page-link" aria-label="Next">
+												<span aria-hidden="true">
+													<span class="lnr lnr-arrow-right"></span>
+												</span>
+											</a>
+										</li>
+									</ul>
+								</nav>
+							</div>
+						</div>
+					</section>
+					<!-- Start Post Area -->
+				</div>
+
+                    @include('inc.sidebar')
+				
+			</div>
+		</div>
+	</div>
+	<!-- Start main body Area -->
+
+	<!-- start footer Area -->
+	<footer class="footer-area section-gap">
+		<div class="container box_1170">
+			<div class="row">
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h6 class="footer_title">About Us</h6>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore
+							magna aliqua.</p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="single-footer-widget">
+						<h6 class="footer_title">Newsletter</h6>
+						<p>Stay updated with our latest trends</p>
+						<div id="mc_embed_signup">
+							<form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
+							 method="get" class="subscribe_form relative">
+								<div class="input-group d-flex flex-row">
+									<input name="EMAIL" placeholder="Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address '"
+									 required="" type="email">
+									<button class="btn sub-btn"><span class="lnr lnr-arrow-right"></span></button>
+								</div>
+								<div class="mt-10 info"></div>
+							</form>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 col-sm-6">
+					<div class="single-footer-widget instafeed">
+						<h6 class="footer_title">Instagram Feed</h6>
+						<ul class="list instafeed d-flex flex-wrap">
+							<li><img src="img/i1.jpg" alt=""></li>
+							<li><img src="img/i2.jpg" alt=""></li>
+							<li><img src="img/i3.jpg" alt=""></li>
+							<li><img src="img/i4.jpg" alt=""></li>
+							<li><img src="img/i5.jpg" alt=""></li>
+							<li><img src="img/i6.jpg" alt=""></li>
+							<li><img src="img/i7.jpg" alt=""></li>
+							<li><img src="img/i8.jpg" alt=""></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-6 col-sm-6">
+					<div class="single-footer-widget f_social_wd">
+						<h6 class="footer_title">Follow Us</h6>
+						<p>Let us be social</p>
+						<div class="f_social">
+							<a href="#"><i class="fa fa-facebook"></i></a>
+							<a href="#"><i class="fa fa-twitter"></i></a>
+							<a href="#"><i class="fa fa-dribbble"></i></a>
+							<a href="#"><i class="fa fa-behance"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row footer-bottom d-flex justify-content-between align-items-center">
+				<p class="col-lg-12 footer-text text-center"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+			</div>
+		</div>
+	</footer>
+	<!-- End footer Area -->
+	<script src="{{ asset('js/vendor/jquery-2.2.4.min.js') }}"></script>
+	<script src="{{ asset('js/vendor/bootstrap.min.js') }}"></script>
+	<script src="{{ asset('js/easing.min.js') }}"></script>
+	<script src="{{ asset('js/hoverIntent.js') }}"></script>
+	<script src="{{ asset('js/superfish.min.js') }}"></script>
+	<script src="{{ asset('js/jquery.ajaxchimp.min.js') }}"></script>
+	<script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
+	<script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+	<script src="{{ asset('js/jquery.tabs.min.js') }}"></script>
+	<script src="{{ asset('js/jquery.nice-select.min.js') }}"></script>
+	<script src="{{ asset('js/waypoints.min.js') }}"></script>
+	<script src="{{ asset('js/mail-script.js') }}"></script>
+	<script src="{{ asset('js/main.js') }}"></script>
+	<script src=""></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+	 crossorigin="anonymous"></script>
+	<script src=""></script>
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+	
+</body>
+
+</html>
